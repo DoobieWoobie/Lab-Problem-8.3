edf1bf356eea38466882681bb5ba2b3207ec296e
Andrew Wu <a-wu.1@onu.edu>
2024-04-02 19:52:22 -0400

    completed convertRomanToint function

--- Project19/FileName.cpp
+++ Project19/FileName.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//Roman Character translate function
+//Roman Character Value function
 int romanCharValue(char r)
 {
 	if (r == 'I')
@@ -20,23 +20,50 @@ int romanCharValue(char r)
 		return 1000;
 	return -1;
 }
+//Roman to Integer Function
+int convertRomanToInt(string s)
+{
+	int total = 0;
+	int string = s.size();
+	for (int i = 0; i < string; i++)
+	{
+		int s1 = romanCharValue(s[i]);
+		int s2 = romanCharValue(s[i + 1]);
+		if (i + 1 < s.length())
+		{
+			if (s1 >= s2)
+			{
+				total = total + s1;
+			}
+			else
+			{
+				total = total + s2 - s1;
+				i++;
+			}
+		}
+		else
+		{
+			total = total + s1;
+		}
+	} return total;
+}
+//Main Function
 int main()
 {
 	bool input = true;
 	while (input)
 	{
-
-		char r;
+		string s;
 		cout << "Input Roman Number or Q to quit: ";
-		cin >> r;
-		int result = romanCharValue(r);
-		if (r == 'Q')
+		cin >> s;
+		int result = convertRomanToInt(s);
+		if (s == "Q")
 		{
 			input = false;
 		}
 		else
 		{
-			cout << r << " = " << result << endl;
+			cout << s << " = " << result << endl;
 		}
 	}return 0;
 }
